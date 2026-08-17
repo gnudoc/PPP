3c06984ea4bf08cfb5cd6cb14edcb83f5cb752cf
Aijaz Mohammad <20248043+gnudoc@users.noreply.github.com>
2026-08-17 16:23:48 +0100

    Basic example of try/throw/catch

--- ch4/main.cpp
+++ ch4/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <stdexcept>
 
-int main() {
-    std::cout << "Hello from ch4!\n";
-    return 0;
+auto main() -> int {
+  std::cout << "Hello from ch4!\nHere come the drills!\n";
+  try {
+    bool cond = false;
+    if (!cond) {
+      throw std::runtime_error{"The cond is false"};
+    } else {
+      return 0;
+    }
+  } catch (std::runtime_error &e) {
+    // or use std::exception to catch both runtime_error and out_of_range
+    std::cerr << "error: " << e.what() << '\n';
+    return 1;
+  } catch (...) {
+    std::cerr << "Hmm. Unknown exception.\n";
+    return 2;
+  }
 }
